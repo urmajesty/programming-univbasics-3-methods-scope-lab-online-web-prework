63b0f4c268a90f7a5ddc441c2259ed8cf7ddf737
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-12 04:29:40 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,8 @@
 # def mario
  # status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-#  puts phrase
-#end
-
-# def toadstool
-#  puts status
-# end
+# puts phrase
+end
+ def toadstool
+ puts status
+end
 def link = "It's Dangerous To Go Alone! Take This."
