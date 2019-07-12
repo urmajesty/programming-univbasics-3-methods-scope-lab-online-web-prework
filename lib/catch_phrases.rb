87a70ab6fa8db823fd21e4bacab917efc00ff85b
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-12 04:26:11 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,8 @@
-#def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+# def mario
+ # status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+#  puts phrase
 #end
 
-def toadstool
-  puts status
-end
+# def toadstool
+#  puts status
+# end
